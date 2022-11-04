88e60cd1e26819ec8a0ed6a3d942c422685285c0
rladmstn <oz33177351@gamil.com>
2022-11-04 19:56:20 +0900

    "메인화면 컴포넌트 구현" range slider, button Component

---
+++ src/buttonComponent.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let buttonType = 'primary';
+    export let selectPriceRange = false;
+</script>
+
+<button class={buttonType} class:selectPriceRange={selectPriceRange}>
+    <slot>음식점 찾기</slot>
+</button>
+
+<style>
+    button{
+        border:0;
+        cursor:pointer;
+        border-radius: 6px;
+        padding:8px 12px;
+        font-weight:bold;
+        box-shadow:1px 2px 3px rgb(0,0,1,0.2)
+    }
+    .selectPriceRange {
+        background: #18A8F1;
+        color: white
+    }
+</style>
