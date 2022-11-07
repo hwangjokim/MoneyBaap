24e47cb42083d8aaa0c846113ad0baee9059c867
hwangjo <hg7258@naver.com>
2022-11-08 00:30:26 +0900

    1108

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import MainView from "./MainView.svelte"
 import SearchView from "./SearchView.svelte"
 
-export default{
+export default {
     "/": MainView,
-    "/search" :SearchView
+    "/search": SearchView
 };
