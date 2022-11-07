386e1f0d875a1b6a6b7369ffa25b72ec627015b2
hwangjo <hg7258@naver.com>
2022-11-08 00:33:58 +0900

    1108v2

---
+++ src/Navbar.svelte
@@ -0,0 +1,27 @@
+<!-- component/Navbar.svelte -->
+<header class="navbar">
+<div class="container">
+    <div class="navbar-brand">
+    <div class="navbar-item">
+        <img src="/fork.png" alt="Logo" />
+    </div>
+    </div>
+    <div class="navbar-menu">
+    <div class="navbar-end">
+        <span class="navbar-item">
+        <!-- svelte-ignore security-anchor-rel-noreferrer -->
+        <a
+            class="button is-info"
+            href="https://github.com/hwangjokim/MoneyBaap"
+            target="_blank"
+        >
+            <span class="icon">
+                <i class="fab fa-github"></i>
+            </span>
+            <span>Github</span>
+        </a>
+        </span>
+    </div>
+    </div>
+</div>
+</header>
