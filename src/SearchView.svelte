541676aab8617be6741a342535823d2e1f2a5bea
hwangjo <hg7258@naver.com>
2022-11-08 17:39:33 +0900

    1108

--- src/SearchView.svelte
+++ src/SearchView.svelte
@@ -2,39 +2,41 @@
     import Navbar from "./components/Navbar.svelte";
     import RangeSlider from "./components/rangeSliderComponent.svelte";
     export const title = "Search View";
-    let search_value = "굶어";
+    let search_value = "돈까스";
     let vls = [0, 10000];
 </script>
 
 <section class="hero" style="width: auto;">
-    <div class="hero-head" >
+    <div class="hero-head">
         <Navbar />
     </div>
     <div class="search_bg">
-    <div class="hero-body">
-        
-    <div class="container" style="margin-top: 100px;">
-        <div class="field has-addons" style="width: 700px;
+        <div class="hero-body">
+            <div class="container" style="margin-top: 100px;">
+                <div
+                    class="field has-addons"
+                    style="width: 700px;
         margin-left: auto;
-        margin-right: auto;">
-            <div class="control has-icons-left" style="width: 600px; ">
-                <input
-                    class="input is-focused"
-                    type="text"
-                    placeholder={search_value}
-                />
-                <span class="icon is-small is-left">
-                    <i class="fa fa-search" aria-hidden="true" />
-                </span>
+        margin-right: auto;"
+                >
+                    <div class="control has-icons-left" style="width: 600px; ">
+                        <input
+                            class="input is-focused"
+                            type="text"
+                            placeholder={search_value}
+                        />
+                        <span class="icon is-small is-left">
+                            <i class="fa fa-search" aria-hidden="true" />
+                        </span>
+                    </div>
+                    <div class="control">
+                        <button class="button is-info"> 검색 </button>
+                    </div>
+                </div>
+                <div class="slider" style="margin-top: 50px; width:1000px">
+                    <RangeSlider />
+                </div>
             </div>
-            <div class="control">
-                <button class="button is-info"> 검색 </button>
-            </div>
-        </div>
-        <div class="slider" style="margin-top: 50px; width:1000px">
-            <RangeSlider />
         </div>
     </div>
-</div>
-</div>
 </section>
