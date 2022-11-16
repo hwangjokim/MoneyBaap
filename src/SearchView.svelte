ed1c509d23e570900ab8f66b4b859b7a6fe6dadf
hwangjo <hg7258@naver.com>
2022-11-16 13:45:42 +0900

    ㅎㅇ

--- src/SearchView.svelte
+++ src/SearchView.svelte
@@ -1,64 +1,53 @@
 <script>
-import Navbar from "./components/Navbar.svelte";
-import RangeSlider from "./components/rangeSliderComponent.svelte";
-import WordList from "./components/WordList.svelte";
-import {
-    onMount
-} from "svelte";
-import {
-    apiData,
-    places
-} from "./apis/api.js";
-import {
-    minvlu
-} from "./store.js";
-import {
-    maxvlu
-} from "./store.js";
+    import Navbar from "./components/Navbar.svelte";
+    import RangeSlider from "./components/rangeSliderComponent.svelte";
+    import WordList from "./components/WordList.svelte";
+    import { onMount } from "svelte";
+    import { apiData, places } from "./apis/api.js";
+    import { minvlu } from "./store.js";
+    import { maxvlu } from "./store.js";
 
-export const title = "Search View";
-let searchHint = "남은 메뉴 키워드 : 5/5";
-let words = [];
-let searcher = "";
-let searchButton = "검색";
+    export const title = "Search View";
+    let searchHint = "남은 메뉴 키워드 : 5/5";
+    let words = [];
+    let searcher = "";
+    let searchButton = "검색";
 
-let modifySearchHint = () => {
-    searchHint = "남은 메뉴 키워드 : " + (5 - words.length) + "/5";
-}
-let addWord = () => {
-    if (words.length < 5) {
-        if (searcher.trim() !== "") {
-            words.push(searcher);
-            words = words;
-            modifySearchHint();
-            searchButton = "검색";
+    let modifySearchHint = () => {
+        searchHint = "남은 메뉴 키워드 : " + (5 - words.length) + "/5";
+    };
+    let addWord = () => {
+        if (words.length < 5) {
+            if (searcher.trim() !== "") {
+                words.push(searcher);
+                words = words;
+                modifySearchHint();
+                searchButton = "검색";
+            }
+            searcher = "";
         }
-        searcher = "";
-    }
-};
-let onHandleDelete = (text) => {
-    words = words.filter((element) => element !== text);
-    modifySearchHint();
-};
+    };
 
-onMount(async () => {
-    fetch("http://127.0.0.1:5921/contacts") // backend 레포에서, RestAPI 폴더로 cd 한 뒤 node app.js해서 백 서버 로컬에서 실행해야 작동함 
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            apiData.set(data);
-        }).catch(error => {
-            console.log(error);
-            return [];
-        });
+    let onHandleDelete = (text) => {
+        words = words.filter((element) => element !== text);
+        modifySearchHint();
+    };
 
-});
-
+    onMount(async () => {
+        fetch("http://127.0.0.1:5921/contacts") // backend 레포에서, RestAPI 폴더로 cd 한 뒤 node app.js해서 백 서버 로컬에서 실행해야 작동함
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                apiData.set(data);
+            })
+            .catch((error) => {
+                console.log(error);
+                return [];
+            });
+    });
 </script>
 
-
 <section class="hero" style="width: auto;">
-
     <div class="hero-head">
         <Navbar />
     </div>
@@ -72,7 +61,7 @@ onMount(async () => {
                             type="text"
                             placeholder={searchHint}
                             bind:value={searcher}
-                            />
+                        />
                         <span class="icon is-small is-left">
                             <i class="fa fa-search" aria-hidden="true" />
                         </span>
@@ -87,26 +76,25 @@ onMount(async () => {
                     <WordList {words} {onHandleDelete} />
                 </div>
             </div>
-
             <div class="slider">
                 <RangeSlider bind:$minvlu bind:$maxvlu />
             </div>
+            <div class="sibal">
             <dl>
                 {#each $places as place}
-                <dt>{place.name}  ★ : {place.star} | {place.locate} </dt>
-                {#each place.menus as menu_list}
-                <dd>{menu_list[0]}, {menu_list[1]}</dd>
-                <!-- <img src={menu_list[2]} width="100" height="100" alt="img"> -->
-
-                {/each}
-                <hr />
+                    <dt>{place.name} ★ : {place.star} | {place.locate}</dt>
+                    {#each place.menus as menu_list}
+                        <dd>{menu_list[0]}, {menu_list[1]}</dd>
+                        <!-- <img src={menu_list[2]}  alt="img"> -->
+                    {/each}
+                    <hr />
                 {/each}
             </dl>
+            </div>
         </div>
     </div>
-    
 </section>
 
 <style lang="scss">
-@import "scss/SearchViewCss.scss"
+    @import "scss/SearchViewCss.scss";
 </style>
