249d5e3b368fa54138c1edf1ffe6cfc5e6ab3792
hwangjokim <hg7258@naver.com>
2023-07-13 04:23:28 +0900

    style : 코드 스타일 개선 및 주석

--- src/SearchView.svelte
+++ src/SearchView.svelte
@@ -1,6 +1,6 @@
 <script>
   import Navbar from "./components/Navbar.svelte";
-  import RangeSlider from "./components/rangeSliderComponent.svelte";
+  import RangeSlider from "./components/rangeSliderComponent.svelte"; //레인지 슬라이더 ,라이브러리
   import WordList from "./components/WordList.svelte";
   import { minvlu } from "./store.js";
   import { maxvlu } from "./store.js";
@@ -8,10 +8,10 @@
   import { replace } from "svelte-spa-router";
   import LazyLoad from "@dimfeld/svelte-lazyload";
   import { onMount } from "svelte";
-  //Todo : 1. 별점, 글자 잘리는 거 해결하기
   export const title = "안숭맛춤";
 
   const options = [
+    //라디오 버튼
     {
       value: "low",
       label: "최저가순",
@@ -27,23 +27,24 @@
   ];
   let radios = options[0];
 
-  let searchHint = "남은 메뉴 키워드 : 5/5";
-  let words = [];
-  let searcher = "";
+  let searchHint = "남은 메뉴 키워드 : 5/5"; //검색칸 placeholder ..로 기억
+  let words = []; // 검색 키워드 저장할 배열
+  let searcher = ""; //검색 글자
   let searchButton = "검색";
-  let result = [];
-  let isCheck = true;
-  let orderFlag = 0;
-  let maxValueForSearch = $maxvlu;
-  const alt = "noimg.png";
+  let result = []; //실제 html에서 loop돌 결과
+  let isCheck = true; // 0원보기 체크바스
+  let maxValueForSearch = $maxvlu; // 슬라이더값 20000 설정시, 최대금액 무제한으로 설정하기 위함
+  const alt = "noimg.png"; // 이미지 없을때 기본 출력값
 
   onMount(() => {
+    // RADIOVALUE = 학교정보. 선택 안된 상태로 넘어오면 (검색 페이지에서 새로고침시) -> 초기 화면으로
     if ($radioValue.value == undefined) replace("/");
     changeDisplay();
   });
 
   //promise가 갱신될 때 마다 (어떻게든) 새롭게 서버에서 쿼리보내서 가져오는 느낌
   let onKeyDown = (e) => {
+    //엔터키 누르면 검색버튼 함수 자동실행
     switch (e.keyCode) {
       case 13:
         addWord();
@@ -51,7 +52,7 @@
   };
 
   let changeDisplay = () => {
-    let see = document.getElementById("see0");
+    let see = document.getElementById("see0"); // 0원 보기 체크박스 표시 여부(슬라이더 0에 갖다둬야 표시됨!)
 
     if ($minvlu == 0) {
       see.style.display = "block";
@@ -68,24 +69,26 @@
     changeDisplay();
     promise = [];
     if (words.length < 5) {
+      //5개 다 안찼고
       if (searcher.trim() !== "") {
+        //공백 입력 아니면
         words.push(searcher);
         words = words;
-        modifySearchHint();
+        modifySearchHint(); //남은 키워드수 갱신
         searchButton = "검색";
-        promise = defaultSearch();
+        promise = defaultSearch(); //API CALL
       } else {
         promise = defaultSearch();
       }
-      document.getElementById("lists").scrollTop = 0;
+      document.getElementById("lists").scrollTop = 0; // 스크롤바 맨위로 올려줌
       searcher = "";
     }
   };
 
   let onHandleDelete = (text) => {
     changeDisplay();
-    words = words.filter((element) => element !== text);
-    promise = defaultSearch(); // promise에 대입해서 갱신을 알림
+    words = words.filter((element) => element !== text); //word 리스트에서 삭제
+    promise = defaultSearch(); // promise에 대입해서 갱신을 알림. 스벨트는 대입문만 해도 업데이트 처리 위해 함수 call됨
     modifySearchHint();
   };
 
@@ -108,29 +111,30 @@
       "&sort=" +
       radios.value +
       "&zero=" +
-      isCheck;
+      isCheck; //쿼리쪽은 API 문서로 올릴게요
     for (let i = 0; i < words.length; i++) {
       query += "&search=" + words[i];
     }
-    fetch(query) // backend 레포에서, RestAPI 폴더로 cd 한 뒤 node app.js해서 백 서버 로컬에서 실행해야 작동함
+    fetch(query)
       .then((response) => response.json())
       .then((data) => {
         result = [];
+        console.log(data);
 
         backup.push(...data);
 
-        for (let i = 0; i < backup.length; i += 8)
-          result.push(backup.slice(i, i + 8));
+        console.log(backup);
+
+        for (let i = 0; i < backup.length; i += 5)
+          result.push(backup.slice(i, i + 5)); //이거 해야 LAZYLOAD 정상 작동함.. 이유는 모름...........
+        console.log(result);
         backup = [];
       });
   };
 
   let backup = [];
-  let tempForView = [];
 
-  let promise = defaultSearch();
-
-  let gridCount = () => {};
+  let promise = defaultSearch(); //맨 첫 실행
 </script>
 
 <svelte:head>
@@ -179,6 +183,7 @@
         </div>
         {#each options as option}
           <label class="radio">
+            //라디오 버튼에 바인딩 과정
             <input
               type="radio"
               bind:group={radios}
@@ -193,48 +198,42 @@
 
       <div class="listOfPlace" id="lists">
         <dl>
-          {#await promise then data}
-            {#each result as placer}
-              <LazyLoad>
-                {#each placer as place}
-                  <a href={place.placeLink} target="_blank" rel="noreferrer">
-                    <!-- svelte-ignore a11y-missing-attribute -->
-                    <div class="box">
-                      <slot>
-                        <div class="pic">
-                          {#if place.imgUrl !== null}
-                            <img loading="lazy" src={place.imgUrl} {alt} />
+          {#each result as placer}
+            <LazyLoad>
+              {#each placer as place}
+                <a href={place.placeLink} target="_blank" rel="noreferrer">
+                  <!-- svelte-ignore a11y-missing-attribute -->
+                  <div class="box">
+                    <slot>
+                      <div class="pic">
+                        {#if place.imgUrl !== null}
+                          <img loading="lazy" src={place.imgUrl} {alt} />
+                        {:else}
+                          <img loading="lazy" src={alt} />
+                        {/if}
+                      </div>
+                      <div class="menuInfo">
+                        <slot>
+                          <div class="m_name" style="font-weight:bold">
+                            {place.menuName}
+                          </div>
+                          <ds>{place.price}원 </ds>
+                          <dt>{place.placeName}</dt>
+                          {#if place.distance < 1000}
+                            <dd>★ : {place.stars} | {place.distance} m</dd>
                           {:else}
-                            <img loading="lazy" src={alt} />
+                            <dd>
+                              ★ : {place.stars} | {place.distance / 1000} km
+                            </dd>
                           {/if}
-                        </div>
-                        <div class="menuInfo">
-                          <slot>
-                            <div class="m_name" style="font-weight:bold">
-                              {place.menuName}
-                            </div>
-                            <ds>{place.price}원 </ds>
-                            <dt>{place.placeName}</dt>
-                            {#if place.distance < 1000}
-                              <dd>★ : {place.stars} | {place.distance} m</dd>
-                            {:else}
-                              <dd>
-                                ★ : {place.stars} | {place.distance / 1000} km
-                              </dd>
-                            {/if}
-                            <!-- {:else if place.distance < 1000}
-                              <dd>{place.distance} m</dd>
-                            {:else}
-                              <dd>{place.distance / 1000} km</dd> -->
-                          </slot>
-                        </div>
-                      </slot>
-                    </div>
-                  </a>
-                {/each}
-              </LazyLoad>
-            {/each}
-          {/await}
+                        </slot>
+                      </div>
+                    </slot>
+                  </div>
+                </a>
+              {/each}
+            </LazyLoad>
+          {/each}
         </dl>
       </div>
     </div>
