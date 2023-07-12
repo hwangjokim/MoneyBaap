04f492964a4b2a88a0234d046a6b68b52fab2483
hwangjokim <hg7258@naver.com>
2023-07-12 22:33:38 +0900

    refactor : 코드 중복 제거 및 최적화

--- src/SearchView.svelte
+++ src/SearchView.svelte
@@ -2,7 +2,6 @@
   import Navbar from "./components/Navbar.svelte";
   import RangeSlider from "./components/rangeSliderComponent.svelte";
   import WordList from "./components/WordList.svelte";
-  import { apiData, places } from "./apis/api.js";
   import { minvlu } from "./store.js";
   import { maxvlu } from "./store.js";
   import { radioValue } from "./store.js";
@@ -14,15 +13,15 @@
 
   const options = [
     {
-      value: "0",
+      value: "low",
       label: "최저가순",
     },
     {
-      value: "1",
+      value: "high",
       label: "최고가순",
     },
     {
-      value: "2",
+      value: "dist",
       label: "거리순",
     },
   ];
@@ -41,7 +40,6 @@
   onMount(() => {
     if ($radioValue.value == undefined) replace("/");
     changeDisplay();
-
   });
 
   //promise가 갱신될 때 마다 (어떻게든) 새롭게 서버에서 쿼리보내서 가져오는 느낌
@@ -53,7 +51,6 @@
   };
 
   let changeDisplay = () => {
-    console.log($minvlu)
     let see = document.getElementById("see0");
 
     if ($minvlu == 0) {
@@ -76,9 +73,9 @@
         words = words;
         modifySearchHint();
         searchButton = "검색";
-        promise = doFetch();
+        promise = defaultSearch();
       } else {
-        promise = doReset();
+        promise = defaultSearch();
       }
       document.getElementById("lists").scrollTop = 0;
       searcher = "";
@@ -88,177 +85,44 @@
   let onHandleDelete = (text) => {
     changeDisplay();
     words = words.filter((element) => element !== text);
-    promise = doReset(); // promise에 대입해서 갱신을 알림
+    promise = defaultSearch(); // promise에 대입해서 갱신을 알림
     modifySearchHint();
   };
 
   // 최저가순:0, 최고가순:1 정렬 바꾸기
   let onChangeOrder = () => {
-    console.log(radios.value);
-    orderFlag = radios.value;
-
-    promise = doReset();
+    promise = defaultSearch();
     document.getElementById("lists").scrollTop = 0;
   };
-  /* 
-doReset 함수 : 키워드를 삭제할 때 마다 호출되는 함수 
-*/
-  let doReset = () => {
-    backup = [];
-    if (words.length == 0)
-      defaultSearch(); // 키워드를 모두 지웠다면, 모든 메뉴 검색
-    else {
-      for (let k = 0; k < words.length; k++) {
-        //키워드가 남아있다면, 순회하며 검색함
-        fetch(
-          "http://ec2-15-165-107-63.ap-northeast-2.compute.amazonaws.com/lowPrice/" +
-            $radioValue.value +
-            "?search=" +
-            words[k]
-        ) // backend 레포에서, RestAPI 폴더로 cd 한 뒤 node app.js해서 백 서버 로컬에서 실행해야 작동함
-          .then((response) => response.json())
-          .then((data) => {
-            result = []; // result는 10개식 짤라서 ㄹㅇㄹㅇㄹㅇ 보여줄 배열
-            apiData.set(data);
-            backup.push(...$places); //백업은  result의 자르기 전 버전
-            if (orderFlag != 2) {
-              console.log("Sort with others");
-              backup.sort(function (first, second) {
-                // 백업 배열에서, 가격을 낮은 순서로 정렬해줌
-                return first.price - second.price;
-              });
-            } else if (orderFlag == 2) {
-              console.log("sort with dist");
-              backup.sort(function (first, second) {
-                // 백업 배열에서, 가격을 낮은 순서로 정렬해줌
-                return first.distance - second.distance;
-              });
-            }
-            backup = backup.filter(
-              //중복 제거 (메뉴 이름과 가게이름 같을경우 컷)
-              (value, index, self) =>
-                index ===
-                self.findIndex(
-                  (t) =>
-                    t.placeName === value.placeName && t.name === value.name
-                )
-            );
-
-            if (orderFlag == 1) backup.reverse();
-            if ($maxvlu == 20000) maxValueForSearch = 1000000;
-            else maxValueForSearch = $maxvlu;
-
-            console.log({ orderFlag });
-            tempForView = backup.filter(
-              //tempForView = 백업 배열에서, 가격 범위가 지정된 배열. 얘가 result에 10개단위로 썰려서 들어감
-              (place) =>
-                parseInt(place.price) >= $minvlu &&
-                parseInt(place.price) <= maxValueForSearch
-            );
-            for (
-              let i = 0;
-              i < tempForView.length;
-              i += 10 //result에 10개씩 잘라서 담아줌
-            )
-              result.push(tempForView.slice(i, i + 10));
-          })
-          .catch((error) => {
-            console.log(error);
-            return [];
-          });
-      }
-    }
-  };
-
-  //doFetch = 키워드 추가할때 호출
-  let doFetch = () => {
-    fetch(
-      "http://ec2-15-165-107-63.ap-northeast-2.compute.amazonaws.com/lowPrice/" +
-        $radioValue.value +
-        "?search=" +
-        words[words.length - 1] //words의 맨 마지막 원소로 검색
-    ) // backend 레포에서, RestAPI 폴더로 cd 한 뒤 node app.js해서 백 서버 로컬에서 실행해야 작동함
-      .then((response) => response.json())
-      .then((data) => {
-        result = [];
-        apiData.set(data);
-        backup.push(...$places);
-        backup.sort(function (first, second) {
-          return first.price - second.price;
-        });
-        backup = backup.filter(
-          (value, index, self) =>
-            index ===
-            self.findIndex(
-              (t) => t.placeName === value.placeName && t.name === value.name
-            )
-        );
-
-        //orderFlag가 1이면, 최고가순 정렬
-        if (orderFlag == 1) backup.reverse();
-        console.log({ orderFlag });
-
-        if ($maxvlu == 20000) maxValueForSearch = 1000000;
-        else maxValueForSearch = $maxvlu;
-
-        tempForView = backup.filter(
-          (place) =>
-            parseInt(place.price) >= $minvlu &&
-            parseInt(place.price) <= maxValueForSearch
-        );
-        for (let i = 0; i < tempForView.length; i += 10)
-          result.push(tempForView.slice(i, i + 10));
-      })
-      .catch((error) => {
-        console.log(error);
-        return [];
-      });
-  };
   let defaultSearch = () => {
-    console.log($radioValue.value);
-    fetch(
-      "http://ec2-15-165-107-63.ap-northeast-2.compute.amazonaws.com/lowPrice/" +
-        $radioValue.value +
-        "?search="
-    ) // backend 레포에서, RestAPI 폴더로 cd 한 뒤 node app.js해서 백 서버 로컬에서 실행해야 작동함
+    window.scrollTo(0, 0);
+    if ($maxvlu == 20000) maxValueForSearch = 1000000;
+    else maxValueForSearch = $maxvlu;
+    let query =
+      "http://localhost:8080/low/" +
+      $radioValue.value +
+      "?minValue=" +
+      $minvlu +
+      "&maxValue=" +
+      maxValueForSearch +
+      "&sort=" +
+      radios.value +
+      "&zero=" +
+      isCheck;
+    for (let i = 0; i < words.length; i++) {
+      query += "&search=" + words[i];
+    }
+    fetch(query) // backend 레포에서, RestAPI 폴더로 cd 한 뒤 node app.js해서 백 서버 로컬에서 실행해야 작동함
       .then((response) => response.json())
       .then((data) => {
-        // console.log(data)
         result = [];
 
-        if ($maxvlu == 20000) maxValueForSearch = 1000000;
-        else maxValueForSearch = $maxvlu;
+        backup.push(...data);
 
-        apiData.set(data);
-        backup.push(...$places);
-
-        if (orderFlag != 2) {
-          console.log("Sort with others");
-          backup.sort(function (first, second) {
-            // 백업 배열에서, 가격을 낮은 순서로 정렬해줌
-            return first.price - second.price;
-          });
-        } else if (orderFlag == 2) {
-          console.log("sort with dist");
-          backup.sort(function (first, second) {
-            // 백업 배열에서, 가격을 낮은 순서로 정렬해줌
-            return first.distance - second.distance;
-          });
-        }
-
-        backup = backup.filter(
-          (place) =>
-            parseInt(place.price) >= $minvlu &&
-            parseInt(place.price) <= maxValueForSearch
-        );
-
-        if (orderFlag == 1) backup.reverse();
-
-        for (let i = 0; i < backup.length; i += 10)
-          result.push(backup.slice(i, i + 10));
+        for (let i = 0; i < backup.length; i += 8)
+          result.push(backup.slice(i, i + 8));
         backup = [];
       });
-
   };
 
   let backup = [];
@@ -305,10 +169,10 @@ doReset 함수 : 키워드를 삭제할 때 마다 호출되는 함수
       <div class="slider">
         <RangeSlider bind:$minvlu bind:$maxvlu />
       </div>
-      
+
       <div class="control is-radio">
         <div id="see0" style="display: none;">
-          <label class="checkbox" >
+          <label class="checkbox">
             <input type="checkbox" bind:checked={isCheck} />
             0원 표시하기
           </label>
@@ -333,81 +197,40 @@ doReset 함수 : 키워드를 삭제할 때 마다 호출되는 함수
             {#each result as placer}
               <LazyLoad>
                 {#each placer as place}
-                  {#if isCheck}
-                    <a href={place.link} target="_blank" rel="noreferrer">
-                      <!-- svelte-ignore a11y-missing-attribute -->
-                      <div class="box">
-                        <slot>
-                          <div class="pic">
-                            {#if place.imgUrl !== null}
-                              <img src={place.imgUrl} />
+                  <a href={place.placeLink} target="_blank" rel="noreferrer">
+                    <!-- svelte-ignore a11y-missing-attribute -->
+                    <div class="box">
+                      <slot>
+                        <div class="pic">
+                          {#if place.imgUrl !== null}
+                            <img loading="lazy" src={place.imgUrl} {alt} />
+                          {:else}
+                            <img loading="lazy" src={alt} />
+                          {/if}
+                        </div>
+                        <div class="menuInfo">
+                          <slot>
+                            <div class="m_name" style="font-weight:bold">
+                              {place.menuName}
+                            </div>
+                            <ds>{place.price}원 </ds>
+                            <dt>{place.placeName}</dt>
+                            {#if place.distance < 1000}
+                              <dd>★ : {place.stars} | {place.distance} m</dd>
                             {:else}
-                              <img src={alt} />
+                              <dd>
+                                ★ : {place.stars} | {place.distance / 1000} km
+                              </dd>
                             {/if}
-                          </div>
-                          <div class="menuInfo">
-                            <slot>
-                              <div class="m_name" style="font-weight:bold">
-                                {place.name}
-                              </div>
-                              <ds>{place.price}원 </ds>
-                              <dt>{place.placeName}</dt>
-                              {#if place.star != null}
-                                {#if place.distance < 1000}
-                                  <dd>★ : {place.star} | {place.distance} m</dd>
-                                {:else}
-                                  <dd>
-                                    ★ : {place.star} | {place.distance / 1000} km
-                                  </dd>
-                                {/if}
-                              {:else if place.distance < 1000}
-                                <dd>{place.distance} m</dd>
-                              {:else}
-                                <dd>{place.distance / 1000} km</dd>
-                              {/if}
-                            </slot>
-                          </div>
-                        </slot>
-                      </div>
-                    </a>
-                  {:else if !isCheck && place.price != 0}
-                    <a href={place.link} target="_blank" rel="noreferrer">
-                      <!-- svelte-ignore a11y-missing-attribute -->
-                      <div class="box">
-                        <slot>
-                          <div class="pic">
-                            {#if place.imgUrl !== null}
-                              <img src={place.imgUrl} />
+                            <!-- {:else if place.distance < 1000}
+                              <dd>{place.distance} m</dd>
                             {:else}
-                              <img src={alt} />
-                            {/if}
-                          </div>
-                          <div class="menuInfo">
-                            <slot>
-                              <div class="m_name" style="font-weight:bold">
-                                {place.name}
-                              </div>
-                              <ds>{place.price}원 </ds>
-                              <dt>{place.placeName}</dt>
-                              {#if place.star != null}
-                                {#if place.distance < 1000}
-                                  <dd>★ : {place.star} | {place.distance} m</dd>
-                                {:else}
-                                  <dd>
-                                    ★ : {place.star} | {place.distance / 1000} km
-                                  </dd>
-                                {/if}
-                              {:else if place.distance < 1000}
-                                <dd>{place.distance} m</dd>
-                              {:else}
-                                <dd>{place.distance / 1000} km</dd>
-                              {/if}
-                            </slot>
-                          </div>
-                        </slot>
-                      </div>
-                    </a>
-                  {/if}
+                              <dd>{place.distance / 1000} km</dd> -->
+                          </slot>
+                        </div>
+                      </slot>
+                    </div>
+                  </a>
                 {/each}
               </LazyLoad>
             {/each}
